7eec6702e812a0550e5afe58dacdc0a74a4606f2
Pooja Sunil Bhambid <poojabhambid2004@gmail.com>
2024-08-28 18:58:34 -0700

    connect to mongoose

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB();
+
+
+// (async()=>{
+//     try {
+//       await  mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//     } catch (error) {
+//         console.error("ERROR",error);
+//         throw error;
+//     }
+// })()
