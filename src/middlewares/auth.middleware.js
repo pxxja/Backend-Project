fc906b42d27318ab98f96749f8a93a667c9a5632
Pooja Sunil Bhambid <poojabhambid2004@gmail.com>
2024-09-29 19:19:06 -0700

    changes

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import { User } from "../models/user.model.js";
+import { apiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+
+export const verifyJWT = asyncHandler( async(req, _, next)=>{
+     try {
+        const token = req.cookies?.accessToken ||
+         req.header("Authorization")?.replace("Bearer", "")
+   
+         if (!token) {
+           throw new apiError(401, "unauthorized request")
+         }
+   
+       //verify if the token is correct or not
+       const decodedToken =  jwt.verify(   token , process.env.ACCESS_TOKEN_SECRET)
+   
+       const user = await User.findById(decodedToken?._id)
+       .select("-password -refreshToken")
+   
+       if (!user) {
+           //todo discuss about frontend
+           throw new apiError(401, "Invalid Access Token")
+       }
+   
+       req.user = user;
+       next()
+     } catch (error) {
+        throw new apiError(401, error?.message || "Invalid access token")
+     }
+})
